0829b9cb4e52bd65baf0d602698078da7d5f991f
daniel.manoel <daniellfmanoel@gmail.com>
2024-03-15 19:06:44 -0300

    🚀 feat: [ definido tema e estilização global do projeto ]

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import { Details } from './pages/Details'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
