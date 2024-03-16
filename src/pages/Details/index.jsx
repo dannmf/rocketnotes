6845a0538bc4ad4b494a2f193d40554249cb4751
daniel.manoel <daniellfmanoel@gmail.com>
2024-03-16 15:30:34 -0300

    🚀 feat: [ alinhamento e criacao do content ]

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles' 
+import { Container, Links, Content } from './styles' 
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
@@ -10,20 +10,32 @@ export function Details(){
     <Container>
       <Header />
 
+      <main>
+        <Content>
       <ButtonText title= "Excluir nota" />
+
+      <h1>Introdução ao React</h1>
+      
+      <p>Lorem ipsum dolor sit amet consectetur,
+         adipisicing elit. Magni provident repudiandae repellendus et totam consequatur facere adipisci illum vel.
+         Fugit praesentium quam laboriosam nisi ex sint eum eius non libero?
+      </p>
+
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://www.rocketseat.com.br/</a></li>
         </Links>
       </Section>
-      
+
       <Section title= "Marcadores">
         <Tag title= "Node" />
         <Tag title= "Flutter" />
         <Tag title= "React" />
       </Section>
-      <Button title= "Voltar" />
 
+      <Button title= "Voltar" />
+      </Content>
+      </main>
     </Container>
   )
 }
