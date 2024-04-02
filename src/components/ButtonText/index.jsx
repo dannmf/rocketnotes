53a44e77a91325f0a04a5ad70ee719ffca9080ef
daniel.manoel <daniellfmanoel@gmail.com>
2024-04-02 07:58:58 -0300

    🚀 feat: [ Header, botões laterais e botão de criação de notas ]

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/prop-types */
 import { Container } from './styles'
 
-export function ButtonText({title, ...rest}){
+export function ButtonText({title, isActive = false, ...rest}){
     return(
-        <Container type='button' {...rest}>
+        <Container 
+        type='button'
+        $isactive={isActive.toString()}
+        {...rest}
+        >
             {title}
         </Container>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
