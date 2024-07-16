a831982afcc74a0dbe832d63a497c414c9fdfec0
daniel.manoel <daniellfmanoel@gmail.com>
2024-07-16 19:13:38 -0300

    feat: [ get das tags na Home ]

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Section } from '../../components/Section'
 import { Note } from '../../components/Note'
-
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
 
 import { ButtonText } from '../../components/ButtonText'
 import { FiPlus } from 'react-icons/fi'
 export function Home() {
+    const [tags, setTags] = useState([])
+
+    useEffect(() => {
+        async function fetchTags() {
+            const response = await api.get('/tags')
+            setTags(response.data)
+        }
+
+        fetchTags()
+    }, [])
     return (
         <Container>
             <Brand>
@@ -18,8 +31,18 @@ export function Home() {
 
             <Menu>
                 <li> <ButtonText title={'Todos'} isActive /></li>
-                <li> <ButtonText title={'React'} /></li>
-                <li><ButtonText title={'Node'} /></li>
+
+                {
+                    tags && tags.map(tag => (
+                        <li>
+                            <ButtonText
+                                title={tag.name}
+                            />
+                        </li>
+
+                    ))
+                }
+
             </Menu>
 
             <Search>
